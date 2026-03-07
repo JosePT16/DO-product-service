a4c31ec5f25e30ceffa94e43b942a7e85f3c8bea
Jose Pajuelo <jpajuelo@uchicago.edu>
2026-03-07 15:29:54 -0600

    Add scan report

--- scripts/scan.ps1
+++ scripts/scan.ps1
@@ -3,12 +3,20 @@ param(
     [string]$ReportPath = "reports\\trivy-product-service.txt"
 )
 
+$repoRoot = (Get-Location).Path
 $reportDir = Split-Path $ReportPath
 if ($reportDir -and -not (Test-Path $reportDir)) {
     New-Item -ItemType Directory -Force $reportDir | Out-Null
 }
 
+$safeTag = ($ImageTag -replace "[:/]", "_")
+$tarPath = Join-Path $repoRoot "reports\\$safeTag.tar"
+docker save $ImageTag -o $tarPath
+
 docker run --rm `
-  -v //./pipe/docker_engine://./pipe/docker_engine `
+  -v "${repoRoot}:/work" `
   aquasec/trivy:0.50.2 `
-  image --exit-code 0 --format table $ImageTag | Out-File -FilePath $ReportPath -Encoding utf8
+  image --exit-code 0 --format table --input "/work/reports/$safeTag.tar" `
+  | Out-File -FilePath $ReportPath -Encoding utf8
+
+Remove-Item $tarPath -ErrorAction SilentlyContinue
